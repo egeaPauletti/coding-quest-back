50279cbe8a0e73b92ce537b528856c9b96811659
jmoranj <morandim.joao@gmail.com>
2025-06-05 22:27:53 -0300

    feat: Implement JWT validation middleware
    This commit introduces a middleware function to validate JWT tokens.
    
    - Implemented `validateJwt` middleware (src/controller/auth/validateJwt.js) to verify JWT tokens from the Authorization header.
    - Added the `validateJwt` middleware to the user route (src/routes/userRoutes.js) to protect the route.
    - Utilized `verifyToken` function from `JwtService` to validate the token.
    - Returns a 401 error if no token is found or a 400 error if the token is invalid.
    - Returns a 200 success with the decoded token data if the token is valid.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import validateJwt from "../controller/auth/validateJwt.js";
 import loginUser from "../controller/user/login.js";
 import registerUser from "../controller/user/register.js";
 
@@ -6,5 +7,6 @@ const userRouter = Router();
 
 userRouter.post("/register", registerUser)
 userRouter.post("/login", loginUser)
+userRouter.post("/", validateJwt)
 
 export default userRouter;
